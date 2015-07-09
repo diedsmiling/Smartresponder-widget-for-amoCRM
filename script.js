0be67e47d013073761092fc008f991bf656fb620
lazarev <diedsmiling@gmail.com>
2015-07-09 15:45:34 +0300

    Fixed css path

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 define(['jquery'], function($){
     var SmartresponderWidget = function () {
-    	var self = this;
+    	var _this = this;
 		this.callbacks = {
 			render: function(){
-				console.log('render');
-				return true;
-			},
+                var lang = _this.i18n('userLang');
+                w_code = _this.get_settings().widget_code; //get widget code
+
+                if(typeof(AMOCRM.data.current_card)!='undefined'){
+                    if(AMOCRM.data.current_card.id == 0) {
+                        return false;
+                    } // do not render if action is "add"
+                }
+                _this.render_template({
+                    caption:{
+                        class_name:'js-ac-caption',
+                        html:''
+                    },
+                    body:'',
+                    render :  '\
+                   <div class="sr-form">\
+                       <div class="sr-form-button sr-sub">SEND</div>\
+                       </div>\
+                       <div class="ac-already-subs"></div>\
+                   <link type="text/css" rel="stylesheet" href="/widgets/'+w_code+'/main.css" >'
+                });
+                return true;
+            },
 			init: function(){
 				console.log('init');
 				return true;
