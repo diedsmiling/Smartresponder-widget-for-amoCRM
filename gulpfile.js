44c5b8eb84fd2607b788e7e61bc3b86a1df8e96a
lazarev <diedsmiling@gmail.com>
2015-07-09 17:16:58 +0300

    Gulp watch task inited

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
-var gulp = require('gulp');
-var zip = require('gulp-zip');
-var debug = require('gulp-debug')
+var gulp  = require('gulp'),
+    zip   = require('gulp-zip'),
+    debug = require('gulp-debug'),
+    watch = require('gulp-watch');
 
-gulp.task('default', function () {
+gulp.task('zip_widget', function () {
     return gulp.src('*')
-        .pipe(debug({title: 'unicorn:'}))
+        .pipe(debug({title: 'Tracked:'}))
         .pipe(zip('widget.zip'))
         .pipe(gulp.dest(''));
 });
+
+gulp.task('watch', function(){
+    gulp.watch('*', ['zip_widget'])
+});
+
+gulp.task('default', ['watch']);
