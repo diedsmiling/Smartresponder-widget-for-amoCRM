0a55dfcefb09f499b7e9b8d3b5127ce7d53f0665
Diedsmiling <diedsmiling@gmail.com>
2015-07-13 16:18:17 +0300

    Gulp file configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,41 @@
 var gulp  = require('gulp'),
     zip   = require('gulp-zip'),
+    copy  = require('gulp-copy'),
     debug = require('gulp-debug'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    clean = require('gulp-clean'),
+    buildFiles = ['i18n/**/*', 'images/**/*', 'manifest.json', 'script.js']
 
-gulp.task('zip_widget', function () {
-    return gulp.src('*')
-        .pipe(debug({title: 'Tracked:'}))
+/**
+ * Deletes widget directory and widget zip file
+ */
+gulp.task('clean_up', function(){
+    return gulp.src(['widget', 'widget.zip'])
+        .pipe(debug({title: 'Deleting'}))
+        .pipe(clean({force: true}))
+});
+
+/**
+ * Copies widget files to build directory
+ * @depends clean_up task
+ */
+gulp.task('build_widget', ['clean_up'], function(){
+   return gulp.src(buildFiles).pipe(debug({title: 'Copying:'})).pipe(copy('widget'));
+});
+
+/**
+ *  Zips build directory
+ *  @depends build_widget task
+ */
+gulp.task('zip_widget', ['build_widget'], function () {
+    return gulp.src('widget/**/*')
+        .pipe(debug({title: 'Zipping:'}))
         .pipe(zip('widget.zip'))
         .pipe(gulp.dest(''));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('*', ['zip_widget'])
+    gulp.watch(buildFiles, ['zip_widget'])
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['zip_widget', 'watch']);
