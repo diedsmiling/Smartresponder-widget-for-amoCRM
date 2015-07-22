5d3c003927c4b554d6d38b539c5b8f7257bb955f
Diedsmiling <diedsmiling@gmail.com>
2015-07-22 14:26:58 +0300

    php i18n file

---
+++ i18n/ru.php
@@ -0,0 +1,7 @@
+<?php
+$messages=array (
+  'widget.name' => 'SmartResponder',
+  'widget.short_description' => 'Виджет SmartResponder позволяет подписать ваши контакты на рассылку или же импортировать в группу напрямую из вашего аккаунта amoCRM.',
+  'widget.description' => 'Виджет SmartResponder позволяет подписать ваши контакты на рассылку или же импортировать в группу напрямую из вашего аккаунта amoCRM.',
+  'exceptions.error' => 'Ошибка'
+);
